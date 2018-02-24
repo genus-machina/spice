b6ce9a0f427fc4274304f7165ee6f0415cfc4d7b
Simplex Rimor <simplex.rimor@gmail.com>
2018-02-23 22:57:51 -0500

    Randomly select a provider if one is not named

--- bin/commands/invoke.js
+++ bin/commands/invoke.js
@@ -1,15 +1,18 @@
 const find = require('lodash/find');
 const providers = require('../../src/providers');
+const sample = require('lodash/sample');
 
-exports.command = 'invoke <provider>';
+exports.command = 'invoke [provider]';
 exports.description = 'Invoke a provider';
 
 exports.handler = async (argv) => {
   const name = argv.provider;
-  const provider = find(providers, ['name', name]);
+  let provider = null;
 
-  if (!provider) {
-    throw new Error(`No provider named '${name}'`);
+  if (name) {
+    provider = find(providers, ['name', name]);
+  } else {
+    provider = sample(providers);
   }
 
   return provider.invoke();
