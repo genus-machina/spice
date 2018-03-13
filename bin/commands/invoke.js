a3cfa2e1a7ba7d1efd1231fc544b88907decabd0
Simplex Rimor <simplex.rimor@gmail.com>
2018-03-12 20:21:22 -0400

    Option to run randomly
    This is useful when scheduling execution with something like cron
    and we don't want it to be predictable.

--- bin/commands/invoke.js
+++ bin/commands/invoke.js
@@ -2,12 +2,35 @@ const find = require('lodash/find');
 const providers = require('../../src/providers');
 const sample = require('lodash/sample');
 
-exports.command = 'invoke [provider]';
+const shouldInvoke = (probability) => {
+  if (!probability) {
+    return false;
+  }
+
+  const roll = Math.random();
+  return roll < probability;
+}
+
+exports.command = 'invoke [options] [provider]';
 exports.description = 'Invoke a provider';
+
+exports.builder = {
+  probability: {
+    alias: 'p',
+    description: 'The probability that the invocation will run',
+    type: 'number'
+  }
+};
 
 exports.handler = async (argv) => {
   const name = argv.provider;
+  const probability = argv.probability;
   let provider = null;
+
+  if (!shouldInvoke(probability)) {
+    console.log('Nothing to see here!');
+    return;
+  }
 
   if (name) {
     provider = find(providers, ['name', name]);
